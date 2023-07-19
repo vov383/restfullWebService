227ab2a6c27ce5bca0c71e1a1dabb93369347495
DESKTOP-RE90PR9\dbtjr <dbtjr0682@gmail.com>
2023-07-19 17:18:21 +0900

    0719 강의수강 시작

--- src/main/java/com/example/restfullwebservice/HelloWorldBean.java
+++ src/main/java/com/example/restfullwebservice/HelloWorldBean.java
@@ -1,6 +1,24 @@
 package com.example.restfullwebservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+// lombok : 빈 클래스 만들 때 getter, setter,
+// 생성자, toString() 자동으로 생성해줌.
+// 시간 단축, 생산성 up
+// 이 라이브러리 사용하기 위해서는 인텔리제이 디펜던시 옵션을 켜주어야 함.
+// setting -> build, execution, deployment -> compiler ->annotation processors :
+// 여기서 enable annotation processing 을 활성화.
+
+// lombok이 지원하는 @AllArgsConstructor 와 직접 작성한 생성자가 동시에 존재하면 Bean 오류 발생함.
+// lombok 이라는 플러그인을 설치하면 이 에러를 밑줄로 추가해준다.
+
+// @NoArgsConstructor -> 아무것도 없는 기본 생성자를 사용하고 싶다
 public class HelloWorldBean {
-    public HelloWorldBean(String message) {
-    }
+    private String message;
+
 }
--- src/main/java/com/example/restfullwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfullwebservice/HelloWorldController.java
@@ -2,7 +2,6 @@ package com.example.restfullwebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.jvm.hotspot.HelloWorld;
 
 @RestController
 public class HelloWorldController {
